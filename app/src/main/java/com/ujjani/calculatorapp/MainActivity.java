51177602485b63949e04b6c9a9620f34fd17ae01
Prajwalujjani <prajwalbunty22@gmail.com>
2020-06-08 15:57:56 +0530

    initial commit

---
+++ app/src/main/java/com/ujjani/calculatorapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.ujjani.calculatorapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtEnterPercent, edtEnterNumber;
+    private Button btnCalculate;
+    private TextView txtResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtEnterPercent = findViewById(R.id.edtEnterPercent);
+        edtEnterNumber = findViewById(R.id.edtEnterNumber);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        txtResult = findViewById(R.id.txtResult);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float percentNumericValue = Float.parseFloat(edtEnterPercent.getText().toString());
+                float decimalValue = percentNumericValue/100;
+                float result = decimalValue * Float.parseFloat(edtEnterNumber.getText().toString());
+                txtResult.setText(Float.toString(result));
+            }
+        });
+
+    }
+}
